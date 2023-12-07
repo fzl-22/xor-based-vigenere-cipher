72bf5422bf8244df4118d9b352d6d0061da40724
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-12-07 22:01:45 +0700

    include vigenere header file

--- examples/main.c
+++ examples/main.c
@@ -1,12 +1,18 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
+#include "vigenere-cipher/vigenere-cipher.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("Usage: %s <input_string> <key_string>\n", argv[0]);
+    return 1;
+  }
+
   char *input_string = argv[1];
   char *key_string = argv[2];
-  char *encrypted_string = (char*)malloc(strlen(input_string) + 1);
+  char *encrypted_string = (char *)malloc(strlen(input_string) + 1);
 
   print_string_info("Input String", input_string);
 
@@ -14,7 +20,7 @@ int main(int argc, char *argv[]){
 
   print_string_info("Encrypted String", encrypted_string);
 
-  char *decrypted_string = (char*)malloc(strlen(input_string) + 1);
+  char *decrypted_string = (char *)malloc(strlen(input_string) + 1);
 
   vigenere_cipher(encrypted_string, key_string, decrypted_string);
 
